905828d69e15ae581dca3f9ce76fc7a26b584f30
kuralinux <79849954+kuralinux@users.noreply.github.com>
2021-04-24 14:17:07 +0300

    Add files via upload

---
+++ xbx.py
@@ -0,0 +1,265 @@
+import os, sys, time
+
+#da dastt krdw krd yan brdt nawy mn bina
+#basha?
+
+def logo():
+    print("\u001b[31m")
+    file = open("22.txt","r")
+    print(file.read())
+
+logo()
+print("\u001b[36m")
+print("[1] ddos attack   ")
+print("[2] proxy cloner by kuralinux  ")
+print("[3] bug wordlist_kurdy  ")
+print("[4] facebook cloner  ")
+print("[5] package installer")
+print("[6] exit   ")
+while True:
+    s = "1"
+    ss = "2"
+    sss = "3"
+    ssss = "4"
+    sssss = "5"
+    ssssss = "6"
+    sssssss = input("[>] ")
+    if s == sssssss:
+        os.system("clear")
+        logo()
+        print("\u001b[34m")
+        print("[1] hammer  ")
+        print("[2] Slowloris   ")
+        print("[3] hulk   ")
+        print("[4] Tor’s Hammer")
+        print("[5] GoldenEye ")
+        print("[6] back")
+        while True:
+            a = "1"
+            aa = "2"
+            aaa = "3"
+            aaaa = "4"
+            aaaaa = "5"
+            aaaaaa = "6"
+            aaaaaaa = input("[>] ")
+            if a == aaaaaaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf hammer")
+                os.system("git clone https://github.com/cyweb/hammer.git")
+                os.system("figlet tawaw")
+                time.sleep(3)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif aa == aaaaaaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf slowloris")
+                os.system("git clone https://github.com/gkbrk/slowloris.git")
+                os.system("figlet tawaw")
+                time.sleep(3)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif aaa == aaaaaaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf hulk")
+                os.system("git clone git clone https://github.com/gkbrk/slowloris.git")
+                os.system("filet tawaw")
+                time.sleep(3)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif aaaa == aaaaaaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf torshammer")
+                os.system("git clone https://github.com/dotfighter/torshammer.git")
+                os.system("figlet tawaw")
+                time.sleep(3)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif aaaaa == aaaaaaa:
+                os.system("claer")
+                os.system("cd ..")
+                os.system("rm -rf GoldenEye")
+                os.system("git clone https://github.com/jseidl/GoldenEye.git")
+                os.system("figlet tawaw")
+                time.sleep(3)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif aaaaaa == aaaaaaa:
+                os.system("clear")
+                os.system("python3 xbx.py.pyc")
+    elif ss == sssssss:
+        os.system("clear")
+        logo()
+    elif sss == sssssss:
+        os.system("clear")
+        logo()
+        print("[1] back")
+        print("esta achyta sar wordlist fileaka tkaya boosta")
+        time.sleep(3)
+        os.system("xdg-open https://www.mediafire.com/file/rnst8oqd5zj1ue8/wordlist_kurdy.txt/file")
+        while True:
+            shlinux = "1"
+            shalinux = input("[>] ")
+            if shlinux == shalinux:
+                os.system("clear")
+                os.system("python3 xbx.py.pyc")
+    elif ssss == sssssss:
+        os.system("clear")
+        logo()
+        print("\u001b[33m")
+        print("[1] mbf-2 by YAYAN XD")
+        print("[2] fox by zed(kurd)  ")
+        print("[3] Cr4ck by YAYAN XD  ")
+        print("[4] Craker by YAYAN XD")
+        print("[5] cmbf by YAYAN XD  ")
+        print("[6] Blueforce--Abm by TECH ABM")
+        print("[7] crack-pro by AZIM")
+        print("[8] back")
+        while True:
+            h = "1"
+            hh = "2"
+            hhh = "3"
+            hhhh = "4"
+            hhhhh = "5"
+            hhhhhh = "6"
+            hhhhhhh = "7"
+            hhhhhhhh = "8"
+            shaa = input("[>] ")
+            if h == shaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf mbf-2")
+                os.system("git clone git clone https://github.com/Yayan-XD/mbf-2")
+                os.system("figlet tawaw")
+                time.sleep(3)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif hh == shaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf fox")
+                os.system("https://github.com/968hacker/fox.git")
+                os.system("cd ..")
+                os.system("figlet tawaw")
+                time.sleep(3)
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif hhh == shaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf Cr4ck")
+                os.system("git clone git clone https://github.com/Yayan-XD/Cr4ck")
+                os.system("cd Cr4ck")
+                os.system("bash setup.sh")
+                time.sleep(5)
+                os.system("figlet tawaw")
+                time.sleep(2)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif hhhh == shaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf Craker")
+                os.system("git clone git clone https://github.com/Yayan-XD/Craker")
+                os.system("cd Craker")
+                os.system("pip2 install requests")
+                time.sleep(2)
+                os.system("pip2 install mechanize")
+                time.sleep(2)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif hhhhh == shaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf cmbf")
+                os.system("git clone https://github.com/Yayan-XD/cmbf")
+                os.system("figlet tawaw")
+                time.sleep(3)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif hhhhhh == shaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("rm -rf Blueforce--Abm")
+                os.system("git clone https://github.com/Tech-abm/Blueforce--Abm")
+                os.system("figlet tawaw")
+                time.sleep(3)
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python3 xbx.py.pyc")
+            elif hhhhhhh == shaa:
+                os.system("clear")
+                os.system("cd ..")
+                os.system("git clone https://github.com/Azim-vau/crack-pro.git")
+                os.system("cd ..")
+                os.system("cd xbx")
+                os.system("python xbx.py.pyc")
+            elif hhhhhhhh == shaa:
+                os.system("clear")
+                os.system("python3 xbx.py.pyc")
+    elif sssss == sssssss:
+        os.system("clear")
+        os.system("termux-setup-storage")
+        time.sleep(3)
+        os.system("apt update")
+        os.system("apt upgrade -y")
+        os.system("apt install nmap -y")
+        os.system("apt install hping3 -y")
+        os.system("apt install wget -y")
+        os.system("apt install argparse")
+        os.system("apt install marshal")
+        os.system("apt install scapy")
+        os.system("apt install toilet")
+        os.system("apt install python2")
+        os.system("apt install python")
+        os.system("pip install requests")
+        os.system("pip install phonenumbers")
+        os.system("apt install mpv -y")
+        os.system("pip install tools")
+        os.system("apt install SETAN -y")
+        os.system("apt install nodejs -y")
+        os.system("pip2 install requests")
+        os.system("pip2 install mechanize")
+        os.system("pip install telethon")
+        os.system("apt install o-editor -y")
+        os.system("apt install dnsutils -y")
+        os.system("apt intsall nano -y")
+        os.system("apt install kwrite")
+        os.system("apt install KWrite")
+        os.system("apt install openssl-tool -y")
+        os.system("apt install curl -y")
+        os.system("apt install unzip -y")
+        os.system("apt install zip -y")
+        os.system("apt install sl -y")
+        os.system("apt install tor -y")
+        os.system("apt install php -y")
+        os.system("apt install fish -y")
+        os.system("clear")
+        os.system("figlet TAWAW BW!")
+        print("ests dagariytawaw!")
+        time.sleep(3)
+        os.system("python3 xbx.py.pyc")
+    elif ssssss == sssssss:
+        os.system("clear")
+        print("\u001b[31m")
+        os.system("figlet EXIT!")
+        os.sys.exit()
+
+
+
+
+
+
+
